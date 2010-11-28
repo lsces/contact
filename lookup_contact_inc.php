c9485adc759b60ba9b29b2c78ef59ebbc130882e
Lester Caine <lsces@lsces.co.uk>
2010-11-28 08:34:08 +0000

    Lookup related tasks data See tasks package for more details

--- lookup_contact_inc.php
+++ lookup_contact_inc.php
@@ -9,18 +9,26 @@
  * Initialization
  */
 require_once( CONTACT_PKG_PATH.'Contact.php');
+require_once( TASKS_PKG_PATH.'Tasks.php');
 require_once( LIBERTY_PKG_PATH.'lookup_content_inc.php' );
 
-// if we already have a gContent, we assume someone else created it for us, and has properly loaded everything up.
-if( empty( $gContent ) || !is_object( $gContent ) ) {
-	if( @BitBase::verifyId( $_REQUEST['content_id'] ) ) {
-		$gContent = new Contact( NULL, $_REQUEST['content_id'] );
-		$gContent->load();
-	} else {
-		$gContent = new Contact();
+if ( isset($_REQUEST['content_id']) ) {
+	// if we already have a gContent, we assume someone else created it for us, and has properly loaded everything up.
+	if( empty( $gContent ) || !is_object( $gContent ) ) {
+		if( @BitBase::verifyId( $_REQUEST['content_id'] ) ) {
+			$gContent = new Contact( NULL, $_REQUEST['content_id'] );
+			$gContent->load();
+		} else {
+			$gContent = new Contact();
+		}
+	
+		$gBitSmarty->clear_assign( 'gContent' );
+		$gBitSmarty->assign( 'gContent', $gContent );
 	}
-
-	$gBitSmarty->clear_assign( 'gContent' );
-	$gBitSmarty->assign( 'gContent', $gContent );
+	if( is_object( $gContent ) ) {
+		$gTask = new Tasks( NULL, $_REQUEST['content_id'] );
+		$gTask->mInfo['tasks'] = $gTask->getList( $_REQUEST );
+		$gBitSmarty->assign_by_ref( 'taskInfo', $gTask->mInfo );
+	}
 }
 ?>
