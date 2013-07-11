1923e813045c00cab7b0c7a5798aacd1360e9d09
lsces <lester@lsces.co.uk>
2013-07-11 22:09:19 +0100

    Tidy commenting for phpdoc manual

--- list_contacts.php
+++ list_contacts.php
@@ -1,9 +1,18 @@
 <?php
-// $Header: /cvsroot/bitweaver/_bit_contact/list_contacts.php,v 1.4 2010/02/08 21:27:22 wjames5 Exp $
-// Copyright (c) 2002-2003, Luis Argerich, Garland Foster, Eduardo Polidor, et. al.
-// All Rights Reserved. See below for details and a complete list of authors.
-// Licensed under the GNU LESSER GENERAL PUBLIC LICENSE. See http://www.gnu.org/copyleft/lesser.html for details.
-// Initialization
+/**
+ * @version $Header: /cvsroot/bitweaver/_bit_contact/list_contacts.php,v 1.4 2010/02/08 21:27:22 wjames5 Exp $
+ *
+ * Copyright (c) 2006 bitweaver.org
+ * All Rights Reserved. See below for details and a complete list of authors.
+ * Licensed under the GNU LESSER GENERAL PUBLIC LICENSE. See http://www.gnu.org/copyleft/lesser.html for details
+ *
+ * @package contact
+ * @subpackage functions
+ */
+
+/**
+ * required setup
+ */
 require_once( '../kernel/setup_inc.php' );
 require_once( CONTACT_PKG_PATH.'Contact.php' );
 
